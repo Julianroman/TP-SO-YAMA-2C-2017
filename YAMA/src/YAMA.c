36dc1406bad3989f67021285aa83b483bb26244e
Gonzalo Rochlin <gonzarochlin@gmail.com>
2017-09-03 18:43:43 -0300

    Creo los distintos procesos

---
+++ YAMA/src/YAMA.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : YAMA.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Comienza el proceso YAMA");
+	return EXIT_SUCCESS;
+}
