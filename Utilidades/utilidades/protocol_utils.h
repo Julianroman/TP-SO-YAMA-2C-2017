2d11caab6d0ed019cd4e797904694f3e97bb2311
Ignacio Escobar <ignacionicolas.e.c@gmail.com>
2017-09-15 20:40:33 -0300

    Nuevas utilidades de Protocolo

---
+++ Utilidades/utilidades/protocol_utils.h
@@ -0,0 +1,28 @@
+/*
+ * protocol_utils.h
+ *
+ *  Created on: 15/9/2017
+ *      Author: utnso
+ */
+
+#ifndef UTILIDADES_PROTOCOL_UTILS_H_
+#define UTILIDADES_PROTOCOL_UTILS_H_
+
+#include "protocolo.h"
+
+typedef struct{
+	t_Mensaje estimulo;
+	void*     accion;
+}t_Respuesta;
+
+// Dado un socket, recibe el header de un mensaje enviado
+t_Mensaje recibir_header(int socket);
+
+// Dados un tipo de mensaje y una tabla de acciones
+// ejecuta la accion correspondiente al tipo de mensaje dado
+//		nota: se ejecuta luego de recibir_header(1), y cada accion
+//            tiene la responsabilidad de manejar su payload
+
+void responder_mensaje(t_Mensaje tipoDeMensaje, t_Respuesta* tablaDeAcciones);
+
+#endif /* UTILIDADES_PROTOCOL_UTILS_H_ */
