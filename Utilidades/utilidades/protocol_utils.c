5f491886af122d4b8fef200456502d9ca6041db8
Ignacio Escobar <ignacionicolas.e.c@gmail.com>
2017-09-17 22:17:58 -0300

    Eliminadas funciones de buqueda y ejecucion de las utilidades de protocolo

--- Utilidades/utilidades/protocol_utils.c
+++ Utilidades/utilidades/protocol_utils.c
@@ -1,8 +1,7 @@
 /*
+ * ============================================================================
  * protocol_utils.c
- *
- *  Created on: 17/9/2017
- *      Author: utnso
+ * ============================================================================
  */
 
 #include <stdlib.h>
@@ -13,39 +12,8 @@
 #include <stdio.h>
 
 
-// Cantidad de elementos de un array x
-#define NELEMS(x)  (sizeof(x) / sizeof((x)[0]))
-
 t_Mensaje recibir_header(int socket){
 	t_Mensaje header;
 	recv(socket,&header,sizeof header, 0);
 	return header;
 };
-
-/*
- * Responsabilidad de responder solicitud delegada a cada modulo
- *
- *
-void* buscar_respuesta(t_Mensaje tipoDeMensaje, t_Respuesta* tablaDeAcciones){
-	if(tipoDeMensaje == SOLICITUDPROCESAMIENTO)printf("solicitud");
-	int i, n = NELEMS(tablaDeAcciones);
-	printf("%d",n);
-	for(i=0; i<n; i++){
-		if(tablaDeAcciones[i].estimulo == tipoDeMensaje){
-			return tablaDeAcciones[i].accion;
-		}
-	}
-	return NULL;
-}
-
-void responder_solicitud(t_Mensaje tipoDeMensaje, t_Respuesta* tablaDeAcciones){
-	void (*accion) ();
-	if((accion = buscar_respuesta(tipoDeMensaje,tablaDeAcciones))!=NULL){
-		// ejecuto la interfaz del comando
-		accion();
-	}else{
-		// Sino muestro un error
-		printf("Mensaje desconocido\n");
-	}
-};
-*/
--- Utilidades/utilidades/protocol_utils.h
+++ Utilidades/utilidades/protocol_utils.h
@@ -1,8 +1,7 @@
 /*
+ * ============================================================================
  * protocol_utils.h
- *
- *  Created on: 15/9/2017
- *      Author: utnso
+ * ============================================================================
  */
 
 #ifndef UTILIDADES_PROTOCOL_UTILS_H_
@@ -10,24 +9,7 @@
 
 #include "protocolo.h"
 
-
-typedef struct{
-	t_Mensaje estimulo;
-	void*     accion;
-}t_Respuesta;
-
 // Dado un socket, recibe el header de un mensaje enviado
 t_Mensaje recibir_header(int socket);
 
-
-/*
- * Responsabilidad de responder solicitudes delegada a los modulos correspondientesgit
- *
-// Dados un tipo de mensaje y una tabla de acciones
-// ejecuta la accion correspondiente al tipo de mensaje dado
-//		nota: se ejecuta luego de recibir_header(1), y cada accion
-//            tiene la responsabilidad de manejar su payload
-
-void responder_solicitud(t_Mensaje tipoDeMensaje, t_Respuesta* tablaDeAcciones);
-*/
 #endif /* UTILIDADES_PROTOCOL_UTILS_H_ */
