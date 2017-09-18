9e3646e3f9c26854aed35b2475a3a8baadcbbba7
Gonzalo Rochlin <gonzarochlin@gmail.com>
2017-09-18 19:38:36 -0300

    Hilos en FileSystem para consola y server

--- FileSystem/src/FileSystem.c
+++ FileSystem/src/FileSystem.c
@@ -13,19 +13,32 @@
 #include <stdlib.h>
 #include <commons/log.h>
 #include <commons/config.h>
+#include <pthread.h>
 #include "consola.h"
 
 t_log* log;
-int miIP = 5040;
+int miPuerto = 5040;
 
 int main(void) {
 	puts("Comienza el proceso FileSystem");
-	log = log_create("fileSystem.log", "FileSystem", false, LOG_LEVEL_TRACE);
+	log = log_create("fileSystem.log", "FileSystem", true, LOG_LEVEL_TRACE);
 	//log_trace(log, "MENSAJE");
 
-	servidor(miIP);
+	//servidor(miPuerto);
 
 	//init_consola();
+
+	//Creo el hiloConsola que llama a la funcion init_consola()
+	pthread_t hiloConsola;
+	pthread_create(&hiloConsola, NULL, (void*) init_consola, NULL);
+
+	//Creo el hiloServidor que llama a la funcion servidor(miPuerto)
+	pthread_t hiloServidor;
+	pthread_create(&hiloServidor, NULL, (void*) servidor, miPuerto);
+
+	//El proceso no termina hasta que mueren los dos hilos
+	pthread_join(hiloConsola, NULL);
+	pthread_join(hiloServidor, NULL);
 
 	return EXIT_SUCCESS;
 }
