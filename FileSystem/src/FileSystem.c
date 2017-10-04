24d399f6ead14177ecb679b0604df4cbd2f8f1ff
Gonzalo Rochlin <gonzarochlin@gmail.com>
2017-10-04 00:57:11 -0300

    Agrego funcionalidades al filesystem

--- FileSystem/src/FileSystem.c
+++ FileSystem/src/FileSystem.c
@@ -19,12 +19,36 @@
 t_log* log;
 int miPuerto = 5040;
 int cantBloques = 20;
-int tamanioBloques = 64;
+int tamanioBloques = 1048576; //1MB
 int bitmap[20];
-char* pathBitmap = "Bitmap.bin";
+char* pathBitmap = ""; //Esto deberia estar en la carpeta Metadata
 
 int estadoEstable = 0;
+int formateado = 0;
 //
+
+void almacenarArchivo(char* pathCompleto, char* nombreArchivo, char* tipo){//Y también recibe "Los datos correspondientes"
+	if(strcmp(tipo,"bin") == 0){
+		puts("ARCHIVO BINARIO");
+
+	}
+	else if(strcmp(tipo,"txt") == 0){
+		puts("ARCHIVO DE TEXTO");
+
+	}
+
+}
+
+void formatear(){
+	/*if(existeUnEstadoAnterior){
+		restaurarDesdeAhi
+	}*/
+
+
+	estadoEstable = 1;
+	formateado = 1;
+}
+
 bool validarArchivo(char* pathArchivo){
 	FILE* archivo;
 	archivo = fopen(pathArchivo,"r");
@@ -39,9 +63,9 @@ bool validarArchivo(char* pathArchivo){
 	}
 }
 
-void crearBitmap() {
+void crearBitmap(char* pathArchivo) {
 	FILE* archivoBitmap;
-	archivoBitmap = fopen(pathBitmap,"w");
+	archivoBitmap = fopen(pathArchivo,"w");
 	int i;
 
 	for (i = 0; i < cantBloques; i++) {
@@ -51,15 +75,21 @@ void crearBitmap() {
 	fclose(archivoBitmap);
 }
 
-void inicializarBitmap() {
-	if(validarArchivo(pathBitmap) == 1){
+void inicializarBitmap(char* nombreNodo) {
+	char *pathNewBitmap = string_new();
+	strcpy(pathNewBitmap, pathBitmap);
+	string_append(&pathNewBitmap, nombreNodo);
+	string_append(&pathNewBitmap, ".bin");
+
+	if(validarArchivo(pathNewBitmap) == 1){
+
 		FILE* archivoBitmap;
-		archivoBitmap = fopen(pathBitmap,"r");
+		archivoBitmap = fopen(pathNewBitmap,"r");
 
 		fgets(bitmap, sizeof(bitmap), archivoBitmap);
 		bitmap[strlen(bitmap)] = '\0';
 	}else{
-		crearBitmap();
+		crearBitmap(pathNewBitmap);
 	}
 
 }
@@ -74,7 +104,7 @@ int main(void) {
 	//init_consola();
 
 	///Creo el hiloConsola que llama a la funcion init_consola()
-	pthread_t hiloConsola;
+	/*pthread_t hiloConsola;
 	pthread_create(&hiloConsola, NULL, (void*) init_consola, NULL);
 
 	//Creo el hiloServidor que llama a la funcion servidor(miPuerto)
@@ -83,13 +113,17 @@ int main(void) {
 
 	//El procSso no termina hasta que mueren los dos hilos
 	pthread_join(hiloConsola, NULL);
-	pthread_join(hiloServidor, NULL);
+	pthread_join(hiloServidor, NULL);*/
 
 	//Para las conexiones, mas adelante falta agregar que si
 	//estadoEstable == 0
 	//No permita conexiones de Workers o YAMA
 
-	inicializarBitmap();
+
+
+
+	inicializarBitmap("Nodo1");
+	almacenarArchivo("","","bin");
 
 	return EXIT_SUCCESS;
 }
