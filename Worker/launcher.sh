743634ab2b909b46efeb135bfdafd1cb9a1a23e0
Ignacio Escobar <ignacionicolas.e.c@gmail.com>
2017-10-06 23:22:08 -0300

    Launcher de multiples procesos Worker

---
+++ Worker/launcher.sh
@@ -0,0 +1,5 @@
+# Abrir instancias del proceso Worker
+python test_launcher/terminal.py --wait Debug/Worker
+python test_launcher/terminal.py --wait Debug/Worker
+python test_launcher/terminal.py --wait Debug/Worker
+
