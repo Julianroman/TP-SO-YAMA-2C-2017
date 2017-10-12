4a64f16eda5cadca8d341083bd2b3bf69db266b0
julianroman <julianromane@gmail.com>
2017-10-11 21:32:40 -0300

    Nocion de Job

---
+++ YAMA/src/Job.h
@@ -0,0 +1,21 @@
+/*
+ * Job.h
+ *
+ *  Created on: 11/10/2017
+ *      Author: utnso
+ */
+
+#ifndef JOB_H_
+#define JOB_H_
+
+#include "Tarea.h"
+
+typedef struct{
+	t_tarea *transformacion;
+	t_tarea *reduccion_local;
+	t_tarea *reduccion_global;
+}t_job;
+
+t_job *newJob();
+
+#endif /* JOB_H_ */
