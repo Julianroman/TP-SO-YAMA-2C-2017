d8526bf11f7a0a5bbec98fdb7bc14f60cd4f41d2
Ignacio Escobar <ignacionicolas.e.c@gmail.com>
2017-10-16 00:47:39 -0300

    Fix de protocolo

--- Utilidades/utilidades/protocol/senders.c
+++ Utilidades/utilidades/protocol/senders.c
@@ -2,7 +2,11 @@
  * senders.c
  * Generado automaticamente por ProtoCool
 */
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
