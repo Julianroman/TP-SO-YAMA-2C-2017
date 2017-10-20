e3a7b61f7c576e676293b64721a1a5364e827740
Ignacio Escobar <ignacionicolas.e.c@gmail.com>
2017-10-19 23:56:11 -0300

    Manejo multihilo de ordenes

--- Master/src/etapas/etapa_almacenamiento.c
+++ Master/src/etapas/etapa_almacenamiento.c
@@ -8,7 +8,9 @@
 #include <commons/log.h>
 #include "etapas.h"
 
-MASTER_STATUS etapa_almacenamiento (int socketYAMA,t_log* logger){
+extern t_log* logger;
+
+MASTER_STATUS etapa_almacenamiento (int socketYAMA){
 	log_trace(logger, "Iniciando etapa de almacenamiento...");
 
 	HEADER_T header;
