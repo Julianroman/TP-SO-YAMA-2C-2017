67cdd4714560139de73a43b8a5746927416ebd44
Ignacio Escobar <ignacionicolas.e.c@gmail.com>
2017-10-19 23:56:38 -0300

    Manejo multiproceso de ordenes

--- Worker/src/serverWorker/responses/res_ORDEN_REDUCCIONLOCAL.c
+++ Worker/src/serverWorker/responses/res_ORDEN_REDUCCIONLOCAL.c
@@ -8,8 +8,12 @@
 #include <utilidades/protocol/senders.h>
 #include <utilidades/protocol/types.h>
 #include <utilidades/protocol/receive.h>
+#include <commons/log.h>
+
+extern t_log* logger;
 
 void res_ORDEN_REDUCCIONLOCAL(int socket_cliente,HEADER_T header,void* data){
-	printf("Orden de reduccion local recibida");
+	log_info(logger, "Respondiendo ORDEN_REDUCCIONLOCAL");
+	send_FIN_LISTA(socket_cliente);
 };
 
